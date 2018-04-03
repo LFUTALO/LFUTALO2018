77a75e0c14822c267fa3e5cb8b36d6d4369d4829
LFUTALO <futalo@ukr.net>
2018-04-03 15:20:32 +0300

    add ignore

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write
+        // write rtrtrtrtr
+        //fgdfdfdfgdfgdfgdfgdfg
         //
-        //
-        //
+        //w
         // ;lk,ljmkjhjghfvgcdfxdgcfvhgbjhyour code here
     }
 }
